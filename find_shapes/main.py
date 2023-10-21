cc6c091ca20f43927d158f72e0bbfba2c003297b
raphael <GitRaphael>
2023-10-21 15:49:41 +0200

    testing camera class in main

--- find_shapes/main.py
+++ find_shapes/main.py
@@ -1,15 +1,21 @@
-# import files
-import file1
-import file2
+"""
+added code for testing class camera.py
+
+"""
+
+
+# import class
+from camera import Camera
 
 def main():
-    # Verwenden Sie die importierten Funktionen oder Klassen
-    img = function_in_file1_getImage()
-    obj = ClassInFile2()
-    obj.some_method()
-
-
-
+    #testing camera class
+    camera = Camera()
+    img = camera.requestImage()
+    camera.showImage(img)
+    path = "../find-shapes/data/sample_image.jpg"
+    sample = camera.requestSample(path)
+    camera.showImage(sample)
+    camera.release()
 
 
 if __name__ == "__main__":
