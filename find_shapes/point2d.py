59fd647ce4b7f3d8fa6bfa15105197ac3bfd8791
Raphael Seitz <r.seitz2001@gmail.com>
2023-10-27 20:56:31 +0200

    created

---
+++ find_shapes/point2d.py
@@ -0,0 +1,25 @@
+"""
+point.py
+
+class Point2D:
+containing x and y coordinate in 2 dimensions
+
+changes:
+1.0     2023-10-27      created
+
+public functions:
+    __init__(self, x=0, y=0):   constructor, taking optional coordinates
+
+public attributes:
+    x: x-coordinate
+    y: y-coordinate
+
+"""
+
+
+class Point2D:
+    __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
