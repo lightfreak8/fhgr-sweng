1532fb5a4e72f31122d7844f984d5410bd3e877b
Raphael Seitz <r.seitz2001@gmail.com>
2023-10-27 21:45:05 +0200

    add __str__

--- find_shapes/point.py
+++ find_shapes/point.py
@@ -9,6 +9,7 @@ changes:
 
 public functions:
     __init__(self, x=0, y=0):   constructor, taking optional coordinates
+    __str__(self):              string representation
 
 public attributes:
     x: x-coordinate
@@ -21,5 +22,8 @@ class Point:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
 
 
