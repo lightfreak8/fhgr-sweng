1ce8926f193011911cf6aa8e24864dab76303571
Raphael Seitz <r.seitz2001@gmail.com>
2023-10-27 21:21:12 +0200

    add point test

--- find_shapes/main.py
+++ find_shapes/main.py
@@ -6,8 +6,19 @@ added code for testing class camera.py
 
 # import class
 from camera import Camera
+from point import Point
+
+def test_point():
+    point = Point(2, 4)
+    print(f'Point = [{point.x}, {point.y}]')
+    point.x = 4.3
+    point.y = 2.8
+    print(f'Point = [{point.x}, {point.y}]')
 
 def main():
+    # testing point class
+    test_point()
+
     #testing camera class
     camera = Camera()
     img = camera.requestImage()
