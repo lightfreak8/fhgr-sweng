af5ecefa019283927351c67176cc83fd0a101ff2
rseitz01 <145366337+rseitz01@users.noreply.github.com>
2023-10-29 16:48:37 +0100

    Revert "F circle (testing)"

--- find_shapes/main.py
+++ find_shapes/main.py
@@ -10,7 +10,6 @@ from point import Point
 from shape import Shape
 from polygon import Polygon
 from circle import Circle
-from image_processor import ImageProcessor
 
 def test_point():
     point = Point(2, 4)
@@ -43,24 +42,6 @@ def test_circle():
     circle = Circle(Point(1, 2), 3)
     print(f'Circle: {circle} ({circle.getName()})')
 
-def test_shape():
-    print('=== Shape Test ===')
-    shapes = [Circle(Point(1, 2), 3),
-              Polygon([Point(1, 2), Point(3, 4), Point(5, 6)]),
-              Polygon([Point(1, 2), Point(3, 4), Point(5, 6), Point(7, 8), Point(9, 10), Point(11, 12)]),
-              Polygon([Point(1, 2), Point(3, 4), Point(5, 6), Point(7, 8), Point(9, 10)]),
-              Circle(Point(8, 9), 10.2),
-              Polygon([Point(1, 2), Point(3, 4), Point(5, 6), Point(7, 8), Point(9, 10)]),
-              Polygon([Point(1, 2), Point(3, 4), Point(5, 6), Point(7, 8)]),
-              Polygon([Point(1, 2), Point(3, 4), Point(5, 6), Point(7, 8), Point(9, 10), Point(11, 12), Point(13, 14)])
-             ]
-    for i in range(len(shapes)):
-        print(f'[{i}] : {shapes[i].getName()} -> {shapes[i]}')
-
-
-def test_timestamp():
-    ip = ImageProcessor()
-    print(f'Timestamp: {ip.createTimestamp()}')
 
 def main():
     # testing point class
@@ -71,12 +52,6 @@ def main():
 
     # testing circle class
     test_circle()
-
-    # testing shapes together
-    test_shape()
-
-    # testing timestamp
-    test_timestamp()
 
     #testing camera class
     camera = Camera()
