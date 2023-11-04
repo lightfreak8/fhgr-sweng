e3b7fd6e2bd2cdd74bde6de11193dd56a1e6ed97
Raphael Seitz <r.seitz2001@gmail.com>
2023-11-04 19:30:18 +0100

    shape.py: add getCenter

--- find_shapes/shape.py
+++ find_shapes/shape.py
@@ -6,9 +6,12 @@ shape class template
 
 changes:
 1.0     2023-10-27      created
+1.1     2023-11-04      add getCenter
 
 public functions:
     getName(self):      get name of shape: Circle, Triangle, Rectangle...
+    getCenter(self):    get center of mass (point)
+    getColor(self):     get color (string)
 
 """
 
@@ -17,6 +20,8 @@ from abc import ABC, abstractmethod
 class Shape(ABC):
     @abstractmethod
     def getName(self):
+        pass
+    def getCenter(self):
         pass
     def getColor(self):
         if self.color is None:
