af4c7a0f12dce1f6eab356b81a7a0190a51a74f8
Raphael Seitz <r.seitz2001@gmail.com>
2023-11-05 18:59:21 +0100

    polygon.py: fix function descriptions

--- find_shapes/polygon.py
+++ find_shapes/polygon.py
@@ -7,6 +7,7 @@ polygon containing points
 changes:
 1.0     2023-10-27      created
 1.1     2023-11-04      add: getCenter
+1.1.1   2023-11-05      fix: function descriptions
 
 public functions:
     __init__(self):     constructor, pass list of Point (s)
@@ -23,6 +24,11 @@ import numpy as np
 
 class Polygon(Shape):
     def __init__(self, points: list):
+        """
+        Constructor for the Polygon class
+        Args:
+            points, list of points of the polygon. requires a minimum of 3 points
+        """
         if len(points) < 3:
             raise Exception(f'Too few points to represent a polygon ({len(points)})')
         for point in points:
@@ -32,6 +38,10 @@ class Polygon(Shape):
         self.color = ""
 
     def __str__(self):
+        """
+        Returns:
+            string representation of the polygon
+        """
         s = '['
         first = True
         for point in self.points:
@@ -43,6 +53,11 @@ class Polygon(Shape):
         return s
 
     def getName(self):
+        """
+        Gets the corresponding name of the polygon
+        Returns:
+            String of the polygon name, e.g. 'Triangle', 'Rectangle', 'Pentagon', etc...
+        """
         if len(self.points) == 3:
             return 'Triangle'
         elif len(self.points) == 4:
@@ -56,6 +71,10 @@ class Polygon(Shape):
             #raise Exception(f'Unknown polygon name containing {len(self.points))} points')
 
     def getCenter(self):
+        """
+        Returns:
+            The center of mass of a polygon
+        """
         center = Point()
         for point in self.points:
             center += point
@@ -63,6 +82,10 @@ class Polygon(Shape):
         return center
 
     def getContour(self):
+        """
+        Returns:
+            The contour of the polygon
+        """
         flat = []
         for p in self.points:
             flat.append([p.x, p.y])
